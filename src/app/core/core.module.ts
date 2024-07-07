e576f81fa0cd51ae143730a51880d7fb6cd13777
thobt <thobui97@outlook.com>
2024-07-07 21:47:10 +0700

    add constructor to prevent multiple imports of CoreModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
-
-
 @NgModule({
-  declarations: [
-    NavbarComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NavbarComponent],
+  imports: [CommonModule],
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() coreModule?: CoreModule) {
+    if (coreModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only'
+      );
+    }
+  }
+}
